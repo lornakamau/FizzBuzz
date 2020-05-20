64d1e2708e2a28a615633e6dec844cc07d3aac78
Lorna Kamau <kamaulorna@gmail.com>
2020-05-20 11:47:41 +0300

    add code for fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,10 @@
+list_numbers = list(range(1,101))
+for i in list_numbers:
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
